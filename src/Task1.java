ed77b336e1524490975573da0d9cd1c51fcd2baa
IskanderV1 <ngk5555@yandex.ru>
2022-07-20 14:53:41 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner zv = new Scanner(System.in);
+        int a = zv.nextInt();
+        for (int i = 0; i <= a; i++) {
+            System.out.print("*");
+        }
+
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner jv = new Scanner(System.in);
+    int a = jv.nextInt();
+        Scanner jvm = new Scanner(System.in);
+        int b = jvm.nextInt();
+        for (int i = 0; i <= b; i++) {
+
+
+            for (int t = 0; i <= a; i++) {
+                System.out.print("*");
+            }
+        }
+        }
+    }
+
