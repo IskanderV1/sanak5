c002f3028c29aa88cbcda5a9c69042b7dd6bd182
IskanderV1 <ngk5555@yandex.ru>
2022-07-21 12:24:15 +0300

    h;ldk,phfgkdfmkldfklsfdkmldfkmdf,lpdfvpdvfpodvfpol,dvfpmodfvpmlvdfmpvdfimpdfvkpmvdfkmpdfvkmpmpdfvpdkfv

---
+++ src/Task5.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    public static void main(String[] args) {
+        int[] az = new int[10];
+
+
+
+        for (int i = 0; int g = 2; i < az.length; i + g) {
+            az[i] = i + 2;
+        }
+        for (int m : az) {
+            System.out.println(m);
+        }
+    }
+    }
+
